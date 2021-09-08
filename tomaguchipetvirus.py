958e6300b5881f2700edeac55d965f3471f525b8
aghastmuffin <78246775+aghastmuffin@users.noreply.github.com>
2021-09-07 18:47:20 -0700

    Update tomaguchipetvirus.py

--- tomaguchipetvirus.py
+++ tomaguchipetvirus.py
@@ -8,10 +8,13 @@ happy = int(100)
 food = int(1000)
 sleep = int(100)
 water = int(100)
+def happy():
+    print("   '                '\n \\___________________/ ")
 while True:
     food = (food - 1)
     if food < (750):
         happy = (3)
-        print("   '                '\n \\___________________/ ")
+        
+        
         
         
